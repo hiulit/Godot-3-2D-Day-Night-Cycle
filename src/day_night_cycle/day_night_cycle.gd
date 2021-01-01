d793a42ef6c273ac421201cb64bf21dba41c698b
hiulit <hiulit@gmail.com>
2021-01-01 21:36:04 +0100

    Reorganized the project

---
+++ src/day_night_cycle/day_night_cycle.gd
@@ -0,0 +1,94 @@
+extends CanvasModulate
+
+export (Color) var color_night = Color(0.07, 0.09, 0.38, 1.0)
+export (Color) var color_dawn = Color(0.86, 0.70, 0.70, 1.0)
+export (Color) var color_day = Color(1.0, 1.0, 1.0, 1.0)
+export (Color) var color_dusk = Color(0.59, 0.66, 0.78, 1.0)
+
+onready var color_transition_tween = $color_transition_tween
+
+func _ready():
+	var current_cycle_changed_signal = Time.connect(
+		"current_cycle_changed",
+		self,
+		"_on_current_cycle_changed"
+	)
+
+	if current_cycle_changed_signal != OK:
+		printerr(current_cycle_changed_signal)
+
+	match Time.current_cycle:
+		Time.CycleState.NIGHT:
+			color = color_night
+		Time.CycleState.DAWN:
+			color = color_dawn
+		Time.CycleState.DAY:
+			color = color_day
+		Time.CycleState.DUSK:
+			color = color_dusk
+
+
+# CALLBACKS
+# ---------
+func _on_current_cycle_changed():
+	match Time.current_cycle:
+		Time.CycleState.NIGHT:
+			if not Time.changing_time_manually:
+				color_transition_tween.interpolate_property(
+					self,
+					"color",
+					color_dusk,
+					color_night,
+					Time.state_transition_duration,
+					Tween.TRANS_SINE,
+					Tween.EASE_OUT
+				)
+				color_transition_tween.start()
+			else:
+				color_transition_tween.stop_all()
+				color = color_night
+		Time.CycleState.DAWN:
+			if not Time.changing_time_manually:
+				color_transition_tween.interpolate_property(
+					self,
+					"color",
+					color_night,
+					color_dawn,
+					Time.state_transition_duration,
+					Tween.TRANS_SINE,
+					Tween.EASE_OUT
+				)
+				color_transition_tween.start()
+			else:
+				color_transition_tween.stop_all()
+				color = color_dawn
+		Time.CycleState.DAY:
+			if not Time.changing_time_manually:
+				color_transition_tween.interpolate_property(
+					self,
+					"color",
+					color_dawn,
+					color_day,
+					Time.state_transition_duration,
+					Tween.TRANS_SINE,
+					Tween.EASE_OUT
+				)
+				color_transition_tween.start()
+			else:
+				color_transition_tween.stop_all()
+				color = color_day
+		Time.CycleState.DUSK:
+			if not Time.changing_time_manually:
+				color_transition_tween.interpolate_property(
+					self,
+					"color",
+					color_day,
+					color_dusk,
+					Time.state_transition_duration,
+					Tween.TRANS_SINE,
+					Tween.EASE_OUT
+				)
+				color_transition_tween.start()
+			else:
+				color_transition_tween.stop_all()
+				color = color_dusk
---
+++ src/debug_overlay/debug_overlay.gd
@@ -0,0 +1,107 @@
+extends PopupPanel
+
+onready var time_slider = $VBoxContainer/time_slider
+onready var freeze_time_checkbox = $VBoxContainer/freeze_time_checkbox
+onready var time_label = $VBoxContainer/time_label
+onready var date_label = $VBoxContainer/date_label
+onready var period_label = $VBoxContainer/period_label
+onready var fps_label = $VBoxContainer/fps_label
+
+
+func _ready():
+	var current_minute_changed_signal = Time.connect(
+		"current_minute_changed",
+		self,
+		"_on_minute_changed"
+	)
+
+	var current_hour_changed_signal = Time.connect(
+		"current_hour_changed",
+		self,
+		"_on_hour_changed"
+	)
+
+	var connect_current_cycle_changed_signal = Time.connect(
+		"current_cycle_changed",
+		self,
+		"_on_current_cycle_changed"
+	)
+
+	if current_minute_changed_signal != OK:
+		printerr(current_minute_changed_signal)
+	
+	if current_hour_changed_signal != OK:
+		printerr(current_hour_changed_signal)
+
+	if connect_current_cycle_changed_signal != OK:
+		printerr(connect_current_cycle_changed_signal)
+
+	freeze_time_checkbox.pressed = Time.freeze_time
+
+	_update_time_slider()
+	_update_time_labels()
+
+	call_deferred("popup")
+
+
+func _process(_delta):
+	_update_fps_text()
+
+
+# PRIVATE FUNCTIONS
+# -----------------
+func _update_time_labels():
+	time_label.text = "Time: " + Time.current_time_string()
+	date_label.text = "Date: " + Time.current_date_string()
+	period_label.text = "Period: " + Time.current_cycle_to_string()
+
+
+func _update_fps_text():
+	fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
+
+
+func _update_time_slider():
+	# Update the handle to reflect the current hour.
+	var current_hour_normalized = Time.get_current_hour() / float(Time.HOURS_IN_A_DAY)
+
+	time_slider.value = current_hour_normalized * time_slider.max_value
+
+
+# CALLBACKS
+# ---------
+func _on_minute_changed():
+	_update_time_labels()
+
+
+func _on_hour_changed():
+	_update_time_labels()
+	_update_time_slider()
+
+
+func _on_current_cycle_changed():
+	_update_time_labels()
+
+
+func _on_time_slider_gui_input(event):
+	if event is InputEventMouseButton:
+		# Disable interpolation while we're dragging the slider.
+		if event.is_pressed():
+			Time.changing_time_manually = true
+		else:
+			Time.changing_time_manually = false
+
+			if not time_slider.editable:
+				time_slider.editable = true
+
+
+func _on_time_slider_value_changed(value):
+	var hour = int((value / time_slider.max_value) * Time.HOURS_IN_A_DAY)
+
+	if hour == -1 or hour == 24:
+		time_slider.editable = false
+
+	Time.set_current_hour(hour)
+
+
+func _on_freeze_time_checkbox_toggled(button_pressed):
+	Time.freeze_time = button_pressed
